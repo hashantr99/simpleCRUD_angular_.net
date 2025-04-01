60f172b472a8f67b1cf3e88ba6795291b86a8ab9
hashantr99 <123436883+hashantr99@users.noreply.github.com>
2025-04-02 01:16:47 +0530

    FE and BE structure
    code with frontend and backend structure

---
+++ API/Contactly/Contactly/Models/AddContactRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Contactly.Models
+{
+    public class AddContactRequestDTO
+    {
+        public string Name { get; set; }
+        public string? Email { get; set; }
+        public string Phone { get; set; }
+        public bool Favourite { get; set; }
+    }
+}
---
+++ API/Contactly/Contactly/Models/Domain/Contact.cs
@@ -0,0 +1,23 @@
+﻿/*namespace Contactly.Models.Domain
+{
+    public class Contact
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public string? Email { get; set; }
+        public required string Phone { get; set; }
+        public bool Favourite { get; set; }
+    }
+}*/
+
+namespace Contactly.Models.Domain
+{
+    public class Contact
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string? Email { get; set; }
+        public string Phone { get; set; }
+        public bool Favourite { get; set; }
+    }
+}
